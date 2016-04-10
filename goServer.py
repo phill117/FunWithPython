68075fea1cd427bcb9a1fd5804b4c1c7ffccb6a9
phill117 <phill117@purdue.edu>
2016-04-10 19:11:42 +1000

    start of server things

---
+++ goServer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print('Connection address:' + str(addr))
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: continue
+    print('received data:' + bytes.decode(data))
+    conn.send(data)  # echo
+conn.close()
